90902a5c77cee8492be09591b22cad215f26947d
Yegor Timoshenko <yegortimoshenko@riseup.net>
2018-05-22 17:40:36 +0300

    Add IPv6 support

--- subnetgen.c
+++ subnetgen.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 uint8_t subnetgen_random_uint8() {
@@ -9,8 +10,26 @@ uint8_t subnetgen_random_uint8() {
   return n;
 }
 
-#define SUBNETGEN_MASK "10.%u.0.0/16"
+#define SUBNETGEN_IPV4_MASK "10.%u.0.0/16"
+#define SUBNETGEN_IPV6_MASK "fd%x:%x%x:%x%x::/48"
 
-int main() {
-  printf(SUBNETGEN_MASK "\n", subnetgen_random_uint8());
+int main(int argc, char *argv[]) {
+  for (;;)
+    switch (getopt(argc, argv, "46")) {
+    case '4':
+      printf(SUBNETGEN_IPV4_MASK "\n", subnetgen_random_uint8());
+      break;
+    case '6':
+      printf(SUBNETGEN_IPV6_MASK "\n",
+             subnetgen_random_uint8(),
+             subnetgen_random_uint8(),
+             subnetgen_random_uint8(),
+             subnetgen_random_uint8(),
+             subnetgen_random_uint8());
+      break;
+    case -1:
+      return EXIT_SUCCESS;
+    default:
+      return EXIT_FAILURE;
+    }
 }
